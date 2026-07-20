977559f6b6ec23cf767fa85ec95025048b488360
benoitlub <benoitlubert@gmail.com>
2026-07-21 01:32:28 +0200

    feat(harvest): add mobile HTML harvest loader

---
+++ mobile-v2-harvest-loader.js
@@ -0,0 +1,6 @@
+(function(){
+  const script=document.createElement('script');
+  script.src='./harvest-html.js?v=20260721-html-harvest';
+  script.async=false;
+  document.head.appendChild(script);
+})();
